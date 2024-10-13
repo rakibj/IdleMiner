5aa586fa67795e2d34cf31d6fadce5177f66a82e
Rakib Jahan <rakib@gameloops.io>
2024-10-13 14:41:21 +0600

    open close ui

--- Assets/_Project/Scripts/Upgrades/UpgradeView.cs
+++ Assets/_Project/Scripts/Upgrades/UpgradeView.cs
@@ -1,9 +1,41 @@
+using System;
+using IdleGame.Workers;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace IdleGame.Upgrades
 {
     public class UpgradeView : MonoBehaviour
     {
-        
+        [SerializeField] private Canvas canvas;
+        [SerializeField] private TapReceiver tapReceiver;
+        [SerializeField] private Button closeButton;
+
+        private void Awake()
+        {
+            Hide();
+        }
+
+        private void OnEnable()
+        {
+            tapReceiver.OnTapped += Show;
+            closeButton.onClick.AddListener(Hide);
+        }
+
+        private void OnDisable()
+        {
+            tapReceiver.OnTapped -= Show;
+            closeButton.onClick.RemoveListener(Hide);
+        }
+
+        public void Show()
+        {
+            canvas.enabled = true;
+        }
+
+        public void Hide()
+        {
+            canvas.enabled = false;
+        }
     }
 }
